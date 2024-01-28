1166621c108cbf72e745d3f8b6f0ca183f07fed3
Guilherme Potter Petry <guipotterrp@gmail.com>
2024-01-27 22:32:02 -0300

    Mensagem

--- flutter_modular_example/android/app/src/main/kotlin/br/com/academiadoflutter/flutter_example_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package br.com.academiadoflutter.flutter_example_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter_modular_example/lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_example_example/app/app_module.dart';
-import 'package:flutter_example_example/app/app_widget.dart';
-import 'package:flutter_modular/flutter_modular.dart';
-
-void main() {
-  runApp(ModularApp(module: AppModule(), child: const AppWidget()));
-}
